696d26eb9267c370af7b8336bea8797759ba8cbd
shinji itou <59269235+manaby-kei@users.noreply.github.com>
2025-08-16 14:37:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // script.js
 document.querySelector('#toggle-btn').addEventListener('click', () => {
   document.querySelector('main').classList.toggle('main-bg');
+  
+document.querySelector('#info-btn').addEventListener('click', () => {
+  document.querySelector('#info-text').classList.toggle('hidden');
+});
 });
