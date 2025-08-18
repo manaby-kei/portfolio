faef03d3dae5da09d4d0b199037f0e2b0229985d
shinji itou <59269235+manaby-kei@users.noreply.github.com>
2025-08-18 16:21:25 +0900

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,3 +2,23 @@
 document.querySelector('#info-btn').addEventListener('click', () => {
   document.querySelector('#info-text').classList.toggle('hidden');
 });
+
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const images = document.querySelectorAll(".work-image");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.style.display = "none";
+});
